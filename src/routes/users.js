6a860b49502646b7b781cee016445723d2d461a3
amder111 <52421748+amder111@users.noreply.github.com>
2021-08-25 13:28:48 +0530

    Initial commit

---
+++ src/routes/users.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const passport = require('passport');
+const {
+    profile, 
+    signIn, 
+    signUp, 
+    create, 
+    update,
+    createSession, 
+    destroySession} = require('../controllers/userController');
+
+const router = express.Router();
+
+router.get('/profile/:id', passport.checkAuthentication, profile);
+router.get('/signin', signIn);
+router.get('/signup', signUp);
+router.post('/create', create);
+router.post('/update/:id', passport.checkAuthentication, update);
+router.post('/create-session', passport.authenticate(
+    'local',
+    {
+    failureRedirect: '/signin'}
+),createSession);
+
+router.get('/signout', destroySession);
+
+module.exports = router;
